f9d5a3ab36e375e42d4af67150d4858ba9ec15ec
Jéssica Sobral <85137657+JessSobral@users.noreply.github.com>
2021-12-22 18:49:36 -0300

    Delete python directory

--- python/While, For/for.py
+++
@@ -1,4 +0,0 @@
-tabuada = int(input("Digite um número para exibir a tabuada: "))
-print("Tabuada do número ", tabuada)
-for valor in range(0, 11, 1):
-    print(str(tabuada) + " x " + str(valor) + " = " + str((tabuada * valor)))
--- python/While, For/while.py
+++
@@ -1,4 +0,0 @@
-numero = int(input("Digite um número: "))
-while numero < 100:
-    print("\t" + str(numero))
-    numero = numero +1
