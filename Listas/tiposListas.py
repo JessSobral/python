410194155fe1ecbf36c52142ce532048ccfb0fd4
Jéssica Sobral <85137657+JessSobral@users.noreply.github.com>
2022-01-06 20:00:28 -0300

    Add files via upload

---
+++ Listas/tiposListas.py
@@ -0,0 +1,8 @@
+#lista preenchida estaticamente
+listaEstatica = ["xpto", true]
+
+#lista preenchida dinamicamente
+listaDinamica = [input("Digite o usuário: "), bool(int(input("Está logado? ")))]
+
+#lista vazia
+listaVazia = []
